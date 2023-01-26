b701c7c72987d2960372b93208633deeceefa92a
Alex <juzzle@mail.ru>
2023-01-26 23:16:43 +0300

    test2

--- Home24_01_2023/src/com/company/Main.java
+++ Home24_01_2023/src/com/company/Main.java
@@ -4,26 +4,24 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Main {
-
     public static void main(String[] args) {
-
         parseArrayList(readFile("filename.txt"));
 //        System.out.println( getline(readFile("filename.txt"),1));
-        int [] bbb = {15,4,21,7,19,31,1};
+        int[] bbb = {15, 4, 21, 7, 19, 31, 1};
         BubbleSort(bbb);
-        for (int a: bbb
-             ) {
+        for (int a : bbb
+        ) {
             System.out.print(a + " ");
         }
-
     }
-
     static void parseArrayList(ArrayList<String> arrayList) {  // Метод обработки JSON строк  формата :
         // -> {\"фамилия\":\"Иванов\",\"оценка\":\"5\",\"предмет\":\"Математика\"}
         try {
+
             String[] arrayStBuld = new String[arrayList.size()]; // Создание Массива строк размером принятого листа
             int count = 0;
             for (String st : arrayList                           // Перебераем лист
@@ -34,14 +32,18 @@ public class Main {
                 count++;
             }
             // Перебираем полученые строки и сплитим по символу ","  во временный массив для вывода нужного результата.
+
+
             for (String sts : arrayStBuld
             ) {
-
                 String[] a = sts.split(",");
+
+                // запись в нужный нам файл
                 System.out.println("Студент " + a[0] + " получил " + a[1] + " по предмету " + a[2]);
                 writerFile("Студент " + a[0] + " получил " + a[1] + " по предмету " + a[2], "WriteTest.txt"); // Производим
-                                                                                            // запись в нужный нам файл
+
             }
+
         } catch (Exception exception) {
             logfile(exception.getMessage());
             System.out.println("Возникла ошибка при обработе запроса");
@@ -49,7 +51,7 @@ public class Main {
     }
 
     static ArrayList<String> readFile(String path) { // Метод для считывания из файла строк, возращается ArrayList в
-                                                    // качестве результата.
+        // качестве результата.
         ArrayList arrayLines = new ArrayList();
         try {
             FileReader fileReader = new FileReader(path);
@@ -59,9 +61,10 @@ public class Main {
             while ((line = bufferedReader.readLine()) != null) {
                 arrayLines.add(line);
             }
-        } catch (IOException exception) {
+            logfile("");
+        } catch (Exception exception) {
             logfile(exception.getMessage());
-            exception.printStackTrace();
+
         }
         return arrayLines;
     }
@@ -82,48 +85,55 @@ public class Main {
                 writer.close();
 
             } catch (Exception exception) {
+                logfile(exception.getMessage());
                 System.out.println(exception.getMessage());
             }
         }
     }
 
     static void logfile(String log) {
-
-        writerFile(log, "log.txt");
+        Logger logger = Logger.getAnonymousLogger();
+       if(log == null || log.isEmpty())
+            logger.log(Level.INFO,"Run");
+       else {
+            logger.log(Level.WARNING,log);
+            writerFile(log,"log.txt");
+       }
+//        writerFile(log, "log.txt");
     }
 
     static String getline(ArrayList<String> arraylist, int count) {
-        int cou  = count-1;
+        int cou = count - 1;
         int line = 0;
         String result = "";
-        for (String st: arraylist
-             ){
-                if(cou == line){
-                    result = st;
-                }
-                line++;
+        for (String st : arraylist
+        ) {
+            if (cou == line) {
+                result = st;
+            }
+            line++;
         }
 
         return result;
     }
+
     //   int [] bbb = {31,4,21,7,19,1};
-    static void BubbleSort(int [] array) {
+    static void BubbleSort(int[] array) {
         int tmp = array[0];
         for (int i = 0; i < array.length; i++) {
-            for (int j = 1; j <array.length-i; j++) {
-                writerFile(Arrays.toString(array),"iterationFromBubblesteps.txt");
-                    if(array[j] < array[j-1] ){
-                        tmp = array[j];
-                        array[j] = array[j-1];
-                        array[j-1] = tmp;
+            for (int j = 1; j < array.length - i; j++) {
+                writerFile(Arrays.toString(array), "iterationFromBubblesteps.txt");
+                if (array[j] < array[j - 1]) {
+                    tmp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tmp;
 
-                    }
+                }
 
             }
 
         }
     }
-
 
 }
 
